165943ab563b1e799b3f0185769dd2048053b9b2
woohoocheezy <woohoocheezy@gmail.com>
2022-12-29 23:57:14 +0900

    Make 'house' model

--- config/settings.py
+++ config/settings.py
@@ -29,8 +29,11 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+CUSTOM_APPS = [
+    'houses.apps.HousesConfig',
+]
 
-INSTALLED_APPS = [
+SYSTEM_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
---
+++ houses/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class House(models.Model):
+
+    """House Model"""
+    """Model Definition for Houses"""
+
+
+    name = models.CharField(max_length=140) 
+    price_per_night = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True)
