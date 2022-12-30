377b2971f8ccd60bcab7d68fb5fc47021561bc44
woohoocheezy <woohoocheezy@gmail.com>
2022-12-30 18:21:17 +0900

    add new feature Custom 'User' Model

--- houses/admin.py
+++ houses/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
 from .models import House
 
+
 @admin.register(House)
-class HouseAdmin(admin.ModelAdmin) :
-    pass
+class HouseAdmin(admin.ModelAdmin):
+    # fields = (
+    #     "name",
+    #     "address",
+    #     (
+    #         "price_per_night",
+    #         "pets_allowed",
+    #     ),
+    # )
+
+    list_display = [
+        "name",
+        "price_per_night",
+        "address",
+        "pets_allowed",
+    ]
+
+    list_filter = [
+        "price_per_night",
+        "pets_allowed",
+    ]
+
+    # search_fields = ["address__startswith"]
+    # search_fields = ("address",)
+
+    # exclude = ("price_per_night",)
+
+    # list_display_links = ("name", "address")
+
+    # list_editable = ("pets_allowed",)
--- houses/models.py
+++ houses/models.py
@@ -4,11 +4,18 @@ from django.db import models
 class House(models.Model):
 
     """House Model"""
+
     """Model Definition for Houses"""
 
-
-    name = models.CharField(max_length=140) 
-    price_per_night = models.PositiveIntegerField()
+    name = models.CharField(max_length=140)
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price", help_text="Positive Number Only"
+    )
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pets_allowed = models.BooleanField(default=True)
+    pets_allowed = models.BooleanField(
+        default=True, help_text="Does this house allow pets?"
+    )
+
+    def __str__(self):
+        return self.name
