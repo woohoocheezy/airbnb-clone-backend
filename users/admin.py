f2113df60c28ec07588d3918e0a27a9fa22d8810
woohoocheezy <woohoocheezy@gmail.com>
2022-12-30 19:26:42 +0900

    add new features to User Model like admin panels, fields

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,48 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "username",
+                    "password",
+                    "name",
+                    "email",
+                    "is_host",
+                ),
+                "classes": ("wide",),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+    )
+    # fields = ("email", "password")
+
+    list_display = (
+        "username",
+        "email",
+        "name",
+        "is_host",
+    )
