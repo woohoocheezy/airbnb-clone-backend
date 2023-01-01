83eeb4dc2a973446ba4ede3d6ea7b61e995912fb
woohoocheezy <woohoocheezy@gmail.com>
2023-01-01 23:34:29 +0900

    add ORM and total_amentiries on ROOM

--- rooms/models.py
+++ rooms/models.py
@@ -53,7 +53,10 @@ class Room(CommonModel):
     )
 
     def __str__(self) -> str:
-        return self.name
+        return room.name
+
+    def total_amenitiea(room):
+        return room.amenities.count()
 
 
 class Amenity(CommonModel):
