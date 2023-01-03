f1834f890351c2b420ec26ca6840df3971bd898c
woohoocheezy <woohoocheezy@gmail.com>
2023-01-03 16:52:04 +0900

    add search fields, admin action and custom filters on revies and rooms model admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,10 +2,75 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            (
+                "good",
+                "Good",
+            ),
+            (
+                "great",
+                "Great",
+            ),
+            (
+                "awesome",
+                "Awesome",
+            ),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            return reviews
+
+
+class RatingFilter(admin.SimpleListFilter):
+    title = "Filter by rating"
+
+    parameter_name = "ratingTarget"
+
+    def lookups(self, request, model_admin):
+        return [
+            (
+                "good",
+                "Good",
+            ),
+            (
+                "bad",
+                "Bad",
+            ),
+        ]
+
+    def queryset(self, request, reviews):
+        rating_value = self.value()
+
+        if rating_value:
+            if rating_value == "bad":
+                return reviews.filter(rating__lt=3)
+            else:
+                return reviews.filter(rating__gte=3)
+        else:
+            return reviews
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
         "__str__",
         "payload",
     )
-    list_filter = ("rating",)
+    list_filter = (
+        "rating",
+        RatingFilter,
+        WordFilter,
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+    )
--- rooms/admin.py
+++ rooms/admin.py
@@ -2,12 +2,22 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set all prices to zero")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (reset_prices,)
+
     list_display = (
         "name",
         "price",
         "kind",
+        "rating",
         "total_amenities",
         "owner",
         "created_at",
@@ -24,6 +34,12 @@ class RoomAdmin(admin.ModelAdmin):
         "amenities",
         "created_at",
         "updated_at",
+    )
+
+    search_fields = (
+        "^name",
+        "price",
+        "owner__username",
     )
 
 
