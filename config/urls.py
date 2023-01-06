36c4e2b44e586a735f8acc6d09d498883d52e44f
woohoocheezy <woohoocheezy@gmail.com>
2023-01-06 23:19:06 +0900

    Add serializer for Category with POST requests and is_valid() without DRF's serializer

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("rooms/", include("rooms.urls")),
+    path("categories/", include("categories.urls")),
 ]
