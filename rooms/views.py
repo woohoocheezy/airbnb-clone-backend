2dfde61d869d580235483f3235b079b4f7b569fa
woohoocheezy <woohoocheezy@gmail.com>
2023-01-12 23:46:08 +0900

    Add DELETE and GET(with transaction) api for Room Detail. PUT api left tho

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError
+from rest_framework.exceptions import (
+    NotFound,
+    NotAuthenticated,
+    ParseError,
+    PermissionDenied,
+)
 from rest_framework.status import HTTP_204_NO_CONTENT
+from django.db import transaction
 from .models import Amenity, Room
 from .serializers import AmenitiySerializer, RoomListSerializer, RoomDetailSerializer
 from categories.models import Category
@@ -112,30 +118,25 @@ class Rooms(APIView):
                 except Category.DoesNotExist:
                     raise ParseError("Category not found")
 
-                room = serializer.save(
-                    owner=request.user,
-                    category=category,
-                )
+                # apply django db transaction for amenities
+                try:
+                    with transaction.atomic():
+                        room = serializer.save(
+                            owner=request.user,
+                            category=category,
+                        )
 
-                # get amenity 'pk's from user request
-                amenities = request.data.get("amenities")
+                        # get amenity 'pk's from user request
+                        amenities = request.data.get("amenities")
 
-                # check if 'pk's are in DB
-                for amenity_pk in amenities:
-                    try:
-                        amenity = Amenity.objects.get(pk=amenity_pk)
-                        room.amenities.add(amenity)
-                    except Amenity.DoesNotExist:
-                        pass
+                        # add a amenity to room
+                        for amenity_pk in amenities:
+                            amenity = Amenity.objects.get(pk=amenity_pk)
+                            room.amenities.add(amenity)
 
-                    # try:
-                    #     amenity = Amenity.objects.get(pk=amenity_pk)
-                    # except Amenity.DoesNotExist:
-                    #     raise ParseError(f"Amenity with id {amenity_pk} not found")
-
-                    # room.amenities.add(amenity)
-
-                return Response(RoomDetailSerializer(room).data)
+                        return Response(RoomDetailSerializer(room).data)
+                except Exception:
+                    raise ParseError("Amenity not found")
 
             else:
                 return Response(serializer.errors)
@@ -159,3 +160,30 @@ class RoomDetail(APIView):
         room = self.get_object(pk)
         serializer = RoomDetailSerializer(room)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        room = self.get_object()
+
+        # check if the user is authenticated
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+
+        # check if the user is same with owner of the room
+        if room.user != request.user:
+            raise PermissionDenied
+
+        # update
+
+    def delete(self, request, pk):
+        room = self.get_object(pk)
+
+        # check if the user is authenticated
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+
+        # check if the user is same with the owner of this room
+        if room.owner != request.user:
+            raise PermissionDenied
+
+        room.delete()
+        return Response(HTTP_204_NO_CONTENT)
