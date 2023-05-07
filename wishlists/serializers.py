76dcea20784ce0acd15c56bbd6a937f95f755d68
woohoocheezy <woohoocheezy@gmail.com>
2023-05-07 23:37:28 +0900

    ADD handling of 'GET&POST' request about Wishlist

---
+++ wishlists/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Wishlist
+from rooms.serializers import RoomListSerializer
+
+
+class WishlistSeralizer(ModelSerializer):
+    """The serializer of Wishlist"""
+
+    rooms = RoomListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Wishlist
+        fields = (
+            "name",
+            "rooms",
+            # "experiences",
+        )
--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Wishlist
+from .serializers import WishlistSeralizer
 
-# Create your views here.
+
+class Wishlists(APIView):
+    """the APIView for wishlist"""
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """GET /wishilists
+            Displaying all 'wishlists' that a user created
+
+        Keyword arguments:
+        self --
+        request --
+        Return: the response with the data of wishlist serializer
+        """
+
+        all_wishilists = Wishlist.objects.filter(user=request.user)
+        serializer = WishlistSeralizer(
+            all_wishilists,
+            many=True,
+            context={"request": request},
+        )
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        """POST /wishilists
+           create a 'wishlist' of a user
+
+        Keyword arguments:
+        self --
+        request --
+        Return: the response with the data of wishlist serializer if the serializer is valid or the error
+        """
+        serializer = WishlistSeralizer(data=request.data)
+
+        if serializer.is_valid():
+            wishlist = serializer.save(user=request.user)
+            serializer = WishlistSeralizer(wishlist)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
