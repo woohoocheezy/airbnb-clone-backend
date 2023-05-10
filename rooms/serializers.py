2b3c218d21f9cfaf5083e2576b3bf5acfd795788
woohoocheezy <woohoocheezy@gmail.com>
2023-05-10 11:52:31 +0900

    ADD a feature of 'is_liked' on wishlist

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -5,6 +5,7 @@ from users.serializers import UserSerializerForRoomDetail
 from reviews.serializers import ReviewSerializer
 from categories.serializers import CategorySerializer
 from medias.serializers import PhotoSerializer
+from wishlists.models import Wishlist
 
 
 class AmenitiySerializer(ModelSerializer):
@@ -28,7 +29,9 @@ class RoomDetailSerializer(ModelSerializer):
 
     # average review field
     avg_rating = serializers.SerializerMethodField()
+
     is_owner = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
 
     photos = PhotoSerializer(many=True, read_only=True)
 
@@ -43,6 +46,10 @@ class RoomDetailSerializer(ModelSerializer):
     def get_is_owner(self, room):
         request = self.context["request"]
         return room.owner == request.user
+
+    def get_is_liked(self, room):
+        request = self.context["request"]
+        return Wishlist.objects.filter(user=request.user, rooms__pk=room.pk).exists()
 
     # def create(self, validated_data):
     #     print(validated_data)
